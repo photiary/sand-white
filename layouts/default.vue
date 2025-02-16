de983591a16ff871c5d0b4652e22fc8a30b16bec
funa <photiary@gmail.com>
2025-02-16 22:03:44 +0900

    페이지 작업

---
+++ layouts/default.vue
@@ -0,0 +1,28 @@
+<template>
+  <v-app>
+    <v-app-bar app>
+      <v-container class="flex justify-center gap-8">
+        <v-btn prepend-icon="mdi-aurora" to="/">aurora</v-btn>
+        <v-btn prepend-icon="mdi-weather-snowy" to="/snowy">snowy</v-btn>
+      </v-container>
+    </v-app-bar>
+    <v-main>
+      <v-container>
+        <slot />
+      </v-container>
+    </v-main>
+    <v-footer app>
+      <v-container class="text-right">
+        <p :class="$style.footer">Copywriter-abc v0.0.0</p>
+      </v-container>
+    </v-footer>
+  </v-app>
+</template>
+
+<script setup lang="ts"></script>
+
+<style module lang="scss">
+.footer {
+  @apply text-sand-white-200 font-bold;
+}
+</style>
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,23 @@
 export default {
   content: [],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'sand-white': {
+          50: '#faf7f2',
+          100: '#f4ede0',
+          200: '#ede2cf', // sand-white
+          300: '#d8bf99',
+          400: '#c8a06f',
+          500: '#bc8853',
+          600: '#af7547',
+          700: '#915d3d',
+          800: '#764c36',
+          900: '#60402e',
+          950: '#332017',
+        },
+      },
+    },
   },
   plugins: [],
   important: true, // Tailwind css 우선 https://tailwindcss.com/docs/configuration#important
